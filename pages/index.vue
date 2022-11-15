d9dbbb97c8e5c2e5f52a485a6d3775fd8ed13fa5
Matt Wiggins <matt@consumeandcreate.co>
2022-11-14 19:35:39 -0700

    A good message for now.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <span>mattwiggins.net</span>
+  <span>Think for yourself, question authority. Think for yourself, question authority. Throughout human history, as our species has faced the frightening, terrorizing fact that we do not know who we are, or where we are going in this ocean of chaos, it has been the authorities, the political, the religious, the educational authorities who attempted to comfort us by giving us order, rules, regulations, informing, forming in our minds their view of reality. To think for yourself, you must question authority and learn how to put yourself in a state of vulnerable, open-mindedness; chaotic, confused, vulnerability to inform yourself. Think for yourself, question authority. Think for yourself, question authority. Think for yourself, question authority. Think for yourself... Think for yourself... Order... Regulations... Rules... Think for yourself, question authority. Order... Regulations... Rules... Comfort us...</span>
 </template>
 
 <script lang="ts">
